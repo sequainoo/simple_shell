28d1e719c84600f9cd1849211906e5b3e4165d58
sequainoo <wazatoyung@gmail.com>
2021-10-30 04:46:56 +0000

    Add header and helpers

---
+++ helper.c
@@ -0,0 +1,38 @@
+#include "shell.h"
+
+/**
+ * free_argv - frees any buffer that stores addresses
+ * @argv: a pointer to pointer to char
+ */
+void free_argv(char **argv)
+{
+	char *arg = NULL;
+	int i = 0;
+
+	/* free each block off argv */
+	i = 0;
+	while ((arg = *(argv + i)) != NULL)
+	{
+		free(arg);
+		i++;
+	}
+	i = 0;
+	free(argv);
+}
+
+/**
+ * len_arr_of_ptrs - determines the length of array of pointers
+ * @arr: the null terminated array of pointers
+ * Return: the len of the array or pointer as an int 
+ */
+unsigned int len_arr_of_ptrs(char **arr)
+{
+	unsigned int count = 0;
+
+	while (*arr++)
+	{
+		count++;
+	}
+	
+	return (count);
+}
---
+++ shell.h
@@ -0,0 +1,33 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/* helpers */
+void set_bin_path(char **cmd);
+int count_tokens(char *str);
+char **malloc_tokens(size_t size);
+int set_bounds(char **current_lowerbound, char **current_upperbound);
+/* void parse_token(char const *const str, char **tokens, int n_tokens); */
+void free_argv(char **argv);
+unsigned int is_command(char *cmd);
+int _strcmp(char *str1, char *str2);
+int _strlen(char *str);
+unsigned int len_arr_of_ptrs(char **arr);
+
+/* utils */
+char **_strtok(char *str);
+int _getline(char **lineptr);
+void exec_cmd(char **argv, char **envp);
+void exec_cmd_non_interactive(int argc, char **av, char **envp);
+int prompt(char **lineptr);
+void run_interactive_sh(void);
+void _printenv();
+
+#endif
