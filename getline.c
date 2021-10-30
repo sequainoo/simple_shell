998e00ab03bfbe59578427899158e93999c37953
sequainoo <wazatoyung@gmail.com>
2021-10-30 04:40:17 +0000

    Add getline

---
+++ getline.c
@@ -0,0 +1,51 @@
+#include "shell.h"
+
+/**
+ * _getline - reads line from `stdin`
+ * @lineptr: a pointer to a line / ptr to char *
+ * Return: the number of chars read
+ */
+int _getline(char **lineptr)
+{
+    char *buffer;
+    size_t buffsize = 1024;
+    int chars_read, i = 0;
+
+    buffer = malloc(sizeof(char) * 1024);
+    if (buffer == NULL)
+        return (-1);
+
+    chars_read = read(STDIN_FILENO, buffer, buffsize);
+
+    /* if read() returns 0 then is EOF and no err, dont count*/
+    if (chars_read == 0)
+    {
+        return (chars_read);
+    }
+
+    /* if read() returns -1 is an error return -1 */
+    else if (chars_read == -1)
+    {
+        free(buffer);
+        return (-1);
+    }
+
+    /* remove trailing newline */
+    while (i < buffsize)
+    {
+        if (buffer[i] == '\n')
+            buffer[i] = '\0';
+        i++;
+    }
+
+    /* if chars_read < buffsize realloc to optimise memory space */
+    if (chars_read < buffsize)
+    {
+        buffer = realloc(buffer, sizeof(char) * chars_read + 1);
+        buffer[chars_read] = '\0';
+    }
+
+    *lineptr = buffer;
+
+    return (chars_read);
+}
