e2a5acbb6a08d28c2717795f0429c045b4088003
sequainoo <wazatoyung@gmail.com>
2021-11-04 06:34:57 +0000

    Reset

--- _is_command.c
+++ _is_command.c
@@ -8,24 +8,24 @@
 unsigned int is_command(char *cmd)
 {
 	struct stat st;
-	/*char *cmds[] = {"exit", "env", "cd", "printenv", NULL};
+	char *cmds[] = {"exit", "env", "cd", "printenv", NULL};
 	int len_cmds = len_arr_of_ptrs(cmds);
 	int i = 0, j = 0;
 	int cmd_len = _strlen(cmd);
 	char bin_path[] = "/bin/";
 	size_t len_buff = _strlen(bin_path) + cmd_len + 1;
-	char *path_buff = _malloc_char(len_buff);*/
+	char *path_buff = _malloc_char(len_buff);
 
-	/*while (i < len_cmds)
+	while (i < len_cmds)
 	{
 		if (_strcmp(cmd, cmds[i]))
 			return (1);
 		i++;
-	}*/
+	}
 	if (stat(cmd, &st) == 0)
 		return (1);
 
-	/*i = 0;
+	i = 0;
 
 	while (i < _strlen(bin_path))
 	{
@@ -42,7 +42,7 @@ unsigned int is_command(char *cmd)
 
 	if (stat(path_buff, &st) == 0)
 		return (1);
-	free(path_buff);*/
+	free(path_buff);
 
 	return (0);
 }
